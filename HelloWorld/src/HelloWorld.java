2565924e235ec6e6398dbec422da5ee0dc9c6a29
Abraham <alum.aruizc.2020@iesalixar.org>
2020-12-15 19:44:46 +0100

    2 version

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,8 @@
 public class HelloWorld {
 	// Tu programa comienza con una llamada a main().
     // Imprime "Hola Mundo" a la ventana de la terminal.
-    public static void main(String args[])
+    //ESTO ES UN CAMBIO
+	public static void main(String args[])
     {
         System.out.println("Hola Mundo");
     }
